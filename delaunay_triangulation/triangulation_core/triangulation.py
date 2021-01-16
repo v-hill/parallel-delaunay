b1a644ea60a04bef575be073966b4308dca9ffa3
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-01-16 23:16:04 +0000

    Update triangulation.py

--- delaunay_triangulation/triangulation_core/triangulation.py
+++ delaunay_triangulation/triangulation_core/triangulation.py
@@ -9,51 +9,149 @@ This algorithm computes the Delaunay triangulation of a set of input points.
 # ---------------------------------- Imports ----------------------------------
 
 # Repo module imports
+import triangulation_core.points_tools.split_list as split_list
 import triangulation_core.linear_algebra.linear_algebra as linalg
 from triangulation_core.triangulation_primitives import make_primitives
 
-# ------------------------- Functions of points lists -------------------------
+# --------------------------- Edge finding functions --------------------------
 
-def split_points(input_points):
+def lowest_common_tangent(h_left, h_right):
     """
-    This function takes in a list of points, splits this list in half and
-    return the two new lists containing each subset of the input points.
-    
+    Given two fully triangulated sets of points, this function finds an
+    edge connecting the two triangulations. Each triangulation forms a convex 
+    hull of edges. The edge to be found by this function is the edge with the 
+    lowest y-value point which is still tangential to both hulls. This is
+    known as the 'base' edge, as it is the first edge connecting two 
+    separately triangulated point sets. 
+
     Parameters
     ----------
-    input_points : list
-        The set of points to be split
+    h_left : TriangulationEdges
+    h_right : TriangulationEdges
 
     Returns
     -------
-    left : list
-        The first half of the input points
-    right : list
-        The second half of the input points
+    left_e : int
+        The index of the edge in the right hull which forms one end of the 
+        base edge
+    right_e : int
+        The index of the edge in the left hull which forms the other end of 
+        the base edge
     """
-    mid_val = (len(input_points) + 1) // 2
+    left_e = h_left.outer
+    right_e = h_right.inner
     
-    left = input_points[:mid_val]
-    right = input_points[mid_val:]
-    return left, right
+    pts_left = h_left.points
+    pts_right = h_right.points
+    
+    p1 = pts_left[h_left.edges[left_e].org]
+    p2 = pts_left[h_left.edges[left_e].dest]
 
-def points_splitter_2(points):
-    num = len(points)
-    if num%2==0:
-        return [points[i:i+2] for i in range(0, num, 2)]
-    elif num%2==1:
-        split = [[points[0], points[1], points[3]]]
-        split2 = [points[i:i+2] for i in range(3, num, 2)]
-    return split + split2
+    p4 = pts_right[h_right.edges[right_e].org]
+    p5 = pts_right[h_right.edges[right_e].dest] 
 
-def points_splitter_3(points):
-    num = len(points)
-    if num%3==0:
-        return [points[i:i+3] for i in range(0, num, 3)]
-    elif num%3==1:
-        split = [points[i:i+2] for i in range(0, 4, 2)]
-        split2 = [points[i:i+3] for i in range(4, num, 3)]
-    elif num%3==2:
-        split = [[points[0], points[1]]]
-        split2 = [points[i:i+3] for i in range(2, num, 3)]
-    return split + split2
+    while True:
+        if linalg.on_right(p1, p2, pts_right[h_right.edges[right_e].org]):
+            left_e = h_left.edges[h_left.edges[left_e].sym].onext
+
+            p1 = pts_left[h_left.edges[left_e].org]
+            p2 = pts_left[h_left.edges[left_e].dest]
+
+        elif linalg.on_left(p4, p5, pts_left[h_left.edges[left_e].org]):
+            right_e = h_right.edges[h_right.edges[right_e].sym].oprev
+            p4 = pts_right[h_right.edges[right_e].org]
+            p5 = pts_right[h_right.edges[right_e].dest]
+            
+        else:
+            return left_e, right_e
+
+def rcand_func(rhull, rcand, b1, b2):
+    """
+    This function finds the candidate edge from the right hull triangulation.
+    An initial candidate 'rcand' is given. This candidate is tested. If the
+    candidate fails it is deleted from the triangulation and the next 
+    potential candiate is considered. While a valid candidate has not been 
+    found this process continues until a valid candidate is found.
+
+    Parameters
+    ----------
+    rhull : TriangulationEdges
+        The triangulation of edges on the right hand side
+    rcand : TYPE
+        DESCRIPTION.
+    b1 : list
+        DESCRIPTION.
+    b2 : list
+        DESCRIPTION.
+
+    Returns
+    -------
+    rhull : TriangulationEdges
+        DESCRIPTION.
+    rcand : TYPE
+        DESCRIPTION.
+    """
+    completed = False
+    while not completed:
+        rcand_onext_dest = rhull.edges[rhull.edges[rcand].onext].dest
+        rcand_dest = rhull.edges[rcand].dest
+        ccw_test = linalg.on_right(b1, b2, rhull.points[rcand_onext_dest])
+        next_cand_invalid = linalg.in_circle(b2, b1, 
+                                             rhull.points[rcand_dest], 
+                                             rhull.points[rcand_onext_dest])
+        if ccw_test and next_cand_invalid:
+            t = rhull.edges[rcand].onext
+            rhull.kill_edge(rcand)
+            rcand = t
+        else:
+            completed = True
+    return rhull, rcand
+
+def lcand_func(lhull, lcand, b1, b2):
+    """
+    This function performs the same task as the above 'rcand_func' but testing
+    for the left candidate edge. 
+    """
+    completed = False
+    while not completed:
+        lcand_oprev_dest = lhull.edges[lhull.edges[lcand].oprev].dest
+        lcand_dest = lhull.edges[lcand].dest
+        ccw_test = linalg.on_right(b1, b2, lhull.points[lcand_oprev_dest])
+        next_cand_invalid = linalg.in_circle(b2, b1, 
+                                             lhull.points[lcand_dest], 
+                                             lhull.points[lcand_oprev_dest])
+        if ccw_test and next_cand_invalid:
+            t = lhull.edges[lcand].oprev
+            lhull.kill_edge(lcand)
+            lcand = t
+        else:
+            completed = True
+    return lhull, lcand
+
+def candidate_decider(rcand, lcand, lcand_valid, triangulation):
+    """
+    Given two potential edges which could be added to the triangulation, 
+    decide which of the edges is the correct one to add.
+
+    Parameters
+    ----------
+    rcand : int
+        index of right candidate edge
+    lcand : int
+        index of left candidate edge
+    lcand_valid : TYPE
+        DESCRIPTION.
+    triangulation : TriangulationEdges
+        DESCRIPTION.
+
+    Returns
+    -------
+    result : bool
+        DESCRIPTION.
+    """
+    pt1 = triangulation.points[triangulation.edges[rcand].dest]
+    pt2 = triangulation.points[triangulation.edges[rcand].org]
+    pt3 = triangulation.points[triangulation.edges[lcand].org]
+    pt4 = triangulation.points[triangulation.edges[lcand].dest]
+    result = lcand_valid and linalg.in_circle(pt1, pt2, pt3, pt4)
+    return result
