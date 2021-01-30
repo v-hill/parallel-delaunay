670d5fbf629b03eadf533a179a8198bdca7b0be1
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-01-30 23:59:35 +0000

    Updated correct directory paths

--- delaunay_triangulation/triangulation_mpi_test.py
+++ delaunay_triangulation/triangulation_mpi_test.py
@@ -5,12 +5,12 @@ This script tests the full MPI Delaunay triangulation algorithm.
 from mpi4py import MPI
 
 # Code from local files
-from boids import World
-import generate_values
-from linear_algebra import lexigraphic_sort
-from triangulation import (points_splitter_3, 
-                           make_primitives, 
-                           recursive_group_merge)
+from utilities.settings import World
+import triangulation_core.points_tools.generate_values as generate_values
+from triangulation_core.linear_algebra.linear_algebra import lexigraphic_sort
+# from triangulation_core.triangulation import points_splitter_3
+# from triangulation_core.triangulation import make_primitives
+# from triangulation_core.triangulation import recursive_group_merge
 
 num_points = 10000
 
